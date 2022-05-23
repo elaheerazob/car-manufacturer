e5496e376e84a68c6a48adee56a32c9241e3a17d
elaheerazob <ridwanelahee282@gmail.com>
2022-05-23 22:56:06 +0600

    review update

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,75 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Review from './Review';
 
 const Reviews = () => {
+    const [reviews,SetReviews] = useState([]);
+    useEffect (() =>{
+        const url=('http://localhost:5000/review');
+        fetch(url)
+        .then(res =>res.json())
+        .then(data =>SetReviews(data))
+    } ,[])
     return (
-        <div className='my-10'>
-            <h3 className=' text-center text-primary  text-3xl font-bold uppercase'>Our Reviews</h3>
-            <div className="carousel w-full">
-                <div id="item1" className="flex justify-center carousel-item w-full">
-                <div className="card sm:w-10 lg:w-1/2  lg:card-side bg-base-100 shadow-xl">
-                    <figure><img src="https://i.ibb.co/tsRgSNS/artworks-000239555924-fkas35-t500x500.jpg" alt="Album"/></figure>
-                    <div className="card-body">
-                        <div className="avatar">
-                            <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                             <img src="https://api.lorem.space/image/face?hash=3174" />
-                        </div>
-                        </div>
-                        <h2 className="card-title">Christopher A. Hunt</h2>
-                        <p>The heart and soul of your vehicle is the internal combustion engine. The engine block features parts such as the timing chain, camshaft, crankshaft, spark plugs, cylinder heads, valves and pistons.</p>
-                    </div>
+        <section className='my-28'>
+                <div>
+                    <h4 className="text-xl text-center text-primary font-bold">Client Reviews</h4>
                 </div>
-                </div> 
-                <div id="item2" className=" flex justify-center carousel-item w-full">
-                    <div className="card sm:w-10 lg:w-1/2  lg:card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/JR4bCcq/transmissions.png" alt="Album"/></figure>
-                        <div className="card-body">
-                            <div className="avatar">
-                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img src="https://api.lorem.space/image/face?hash=3174" />
-                            </div>
-                            </div>
-                            <h2 className="card-title">Albert J. Martin</h2>
-                            <p>The heart and soul of your vehicle is the internal combustion engine. The engine block features parts such as the timing chain, camshaft, crankshaft, spark plugs, cylinder heads, valves and pistons.</p>
-                        </div>
-                    </div>
-                </div> 
-                <div id="item3" className="flex justify-center carousel-item w-full">
-                    <div className="card sm:w-10 lg:w-1/2  lg:card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/Pt1VNVB/car-battery-500x500.jpg" alt="Album"/></figure>
-                        <div className="card-body">
-                            <div className="avatar">
-                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img src="https://api.lorem.space/image/face?hash=3174" />
-                            </div>
-                            </div>
-                            <h2 className="card-title">Mark G. Davis</h2>
-                            <p>The heart and soul of your vehicle is the internal combustion engine. The engine block features parts such as the timing chain, camshaft, crankshaft, spark plugs, cylinder heads, valves and pistons.</p>
-                        </div>
-                    </div>
-                </div> 
-                <div id="item4" className="flex justify-center carousel-item w-full">
-                    <div className="card sm:w-10 lg:w-1/2  lg:card-side bg-base-100 shadow-xl">
-                        <figure><img src="https://i.ibb.co/GkCkYC0/maruti-zen-lucas-internal-fan-alternator-500x500.jpg" alt="Album"/></figure>
-                        <div className="card-body">
-                            <div className="avatar">
-                                <div className="w-20 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                                <img src="https://api.lorem.space/image/face?hash=3174" />
-                            </div>
-                            </div>
-                            <h2 className="card-title">Sherman D. Weber</h2>
-                            <p>The heart and soul of your vehicle is the internal combustion engine. The engine block features parts such as the timing chain, camshaft, crankshaft, spark plugs, cylinder heads, valves and pistons.</p>
-                        </div>
-                    </div>
-                </div>
-      </div> 
-      <div className="flex justify-center w-full py-2 gap-2">
-        <a href="#item1" className="btn btn-xs">1</a> 
-        <a href="#item2" className="btn btn-xs">2</a> 
-        <a href="#item3" className="btn btn-xs">3</a> 
-        <a href="#item4" className="btn btn-xs">4</a>
-      </div>
-        </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    reviews.map(review =><Review
+                        key={review._id}
+                        review={review}
+                    ></Review>)
+                }
+            </div>
+        </section>
     );
 };
+
 
 export default Reviews;
